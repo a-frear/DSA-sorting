d97d50e6c0ab140379e9c07db1d3e7c5a57d49e9
a_frear <amy.frear@gmail.com>
2021-03-15 09:36:25 -0400

    Update bucketSort.js

--- bucketSort.js
+++ bucketSort.js
@@ -1,6 +1,7 @@
 //Bucket sort
-const array = [5, 4, 1, 2, 3]
+const array = [25, 4, 1, 12, 3]
 function bucketSort(arr, min, max) {
+    //turn the array into a hashmap
     const map = new Map();
     for (let i = 0; i < arr.length; i++) {
       if (map.get(arr[i]) === undefined) {
@@ -9,16 +10,24 @@ function bucketSort(arr, min, max) {
         map.set(arr[i], map.get(arr[i]) + 1);
       }
     }
+    console.log(arr)
+    //create variable for index 0
     let arrI = 0;
     for (let i = min; i <= max; i++) {
+      //plus one to min, this is not ideal
+      //if the key doesn't exist, move on 
       let numAppearing = map.get(i);
+      console.log('numAppearing before: ' + numAppearing)
+      //if the key exists
       while (numAppearing) {
         arr[arrI] = i;
+        //go back to previous key 
         numAppearing--;
+        //move to next index of array
         arrI++;
       }
     }
     return arr;
   }
   
-console.log(bucketSort(array, 1, 5))
+console.log(bucketSort(array, 1, 25))
